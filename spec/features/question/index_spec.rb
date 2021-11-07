1c169b055f1d5d22faaeb4dc4c9cb4e489783a45
Evgeniy-github-acc <3374555@gmail.com>
2021-11-07 15:35:32 +0300

    questions create, index

---
+++ spec/features/question/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User can view questions', %q{
+  In order to find answer user can view
+  list of all questions which have been
+  created 
+} do
+  given!(:questions) { create_list(:question, 5) }
+  
+  scenario 'visit questions page' do
+    visit questions_path
+    save_and_open_page 
+    
+    questions.each { |question| expect(page).to have_content question.title }
+   
+  end
+end
