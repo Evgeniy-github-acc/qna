9da54dce516c00a50be8afb076e4b873da0ac454
Evgeniy-github-acc <3374555@gmail.com>
2021-11-10 19:58:05 +0300

    add Answer.new spec to quest controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe QuestionsController, type: :controller do
   let(:user){ create(:user) }
   let(:question) { create(:question, author: user) }
   
+  
   describe 'GET #index' do
     let(:questions) { create_list(:question, 3, author: user) }
     before { get :index }
@@ -22,6 +23,10 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'assigns the requested question to @question' do
       expect(assigns(:question)).to eq question
+    end
+
+    it 'assigns new answer for question' do
+      expect(assigns(:answer)).to be_a_new(Answer) 
     end
 
     it 'renders show view' do
