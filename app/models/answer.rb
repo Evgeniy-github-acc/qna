3bb9084e09087177523d27e119506bc9ea2a3a2a
Evgeniy-github-acc <3374555@gmail.com>
2021-12-05 12:08:08 +0300

    linkable concern added

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,9 @@
 class Answer < ApplicationRecord
+  include Linkable
+
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :question
   has_many_attached :files
-  has_many :links, dependent: :destroy, as: :linkable
-
-  accepts_nested_attributes_for :links, reject_if: :all_blank
   
   validates :body, presence: true
 
