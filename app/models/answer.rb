436364a49e0c85b634ab22fb753bd0497cf264a9
Evgeniy <3374555@gmail.com>
2022-10-28 15:48:18 +0300

    add Comment model with specs

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < ApplicationRecord
   include Linkable
   include Votable
+  include Commentable
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :question
