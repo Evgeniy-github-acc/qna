31e39266d96191bdf017431c4f1b61a99220ee8a
Evgeniy <3374555@gmail.com>
2022-11-01 20:49:37 +0300

    create athorization controller and method

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -1,10 +1,21 @@
 class OauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    render json: request.env['omniauth.auth']    
+    provide_auth("Github")
   end
 
   def facebook
-    render json: request.env['omniauth.auth']    
+    provide_auth("Facebook")  
   end
-  
+
+  private
+
+  def provide_auth(kind)
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+    if @user&.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
+    else
+      redirect_to root_path, alert: 'Something weent wrong'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :questions, foreign_key: 'author_id', dependent: :destroy
   has_many :answers, foreign_key: 'author_id', dependent: :destroy
   has_many :awards
+  has_many :authorizations
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
@@ -12,5 +13,21 @@ class User < ApplicationRecord
 
   def author_of?(resource)
     resource.author_id == self.id
-  end       
+  end
+  
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    if user
+      user.authorizations.create(provider: auth.provider, uid: auth.uid) 
+    else
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+      user.authorizations.create(provider: auth.provider, uid: auth.uid)
+    end  
+    user
+  end
+  
 end
