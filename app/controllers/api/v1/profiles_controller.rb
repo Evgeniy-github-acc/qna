ea6ac816f97a2a1672b79c6088fdca9922f46ccf
Evgeniy <3374555@gmail.com>
2022-11-08 11:02:54 +0300

    add list of users to api

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
+  
+  respond_to :json
+  
   def me
-    render json: current_resource_owner
+    respond_with current_resource_owner
+  end
+
+  def index
+    respond_with User.where.not(id: current_resource_owner)
   end
 end
--- spec/api/v1/profile_spec.rb
+++ spec/api/v1/profile_spec.rb
@@ -18,7 +18,8 @@ describe "Profile API" do
     end
     
     context "authorized" do
-      let(:me) { create(:user) } 
+      let(:me) { create(:user) }
+      let(:me_response)  { json["user"] } 
       let(:access_token) { create(:access_token, resource_owner_id: me.id) } 
       before do
         get '/api/v1/profiles/me', params: { access_token: access_token.token }, headers: headers
@@ -30,13 +31,45 @@ describe "Profile API" do
       
       it 'returns all public fields' do
         %w[id email admin created_at updated_at].each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json    
+          expect(me_response[attr]).to eq me.send(attr).as_json    
         end
       end
       
       it 'does not return private fields' do
         %w[password encrypted_password].each do |attr|
           expect(json).to_not have_key(attr)    
+        end
+      end
+    end
+  end
+
+  describe "GET api/v1/profiles" do
+    let(:users_response)  { json["users"] }
+    let!(:users) { create_list(:user, 4) }
+    let(:access_token) { create(:access_token) }
+
+    context "authorized" do
+      before do
+        get '/api/v1/profiles', params: { access_token: access_token.token }, headers: headers
+      end
+
+      it "returns status 200" do
+        expect(response.status).to eq 200
+      end
+      
+      it "returns all users except authorized" do
+        expect(users_response.count).to eq users.count
+      end
+
+      it "returns all neccessary fields" do
+        %w[id email admin created_at updated_at].each do |attr|
+          expect(users_response.first[attr]).to eq User.first.send(attr).as_json
+        end
+      end
+
+      it "does not return private fields" do
+        %w[password encrypted_password].each do |attr|
+          expect(users_response.last).to_not have_key(attr)
         end
       end
     end
